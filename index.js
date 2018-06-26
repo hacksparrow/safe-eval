7051474e413a83ab1d2666329259742f295e88a9
Kauê Gimenes <kaue.gimenes@gmail.com>
2018-06-26 14:48:11 -0300

    Clear Function from eval context

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ module.exports = function safeEval (code, context, opts) {
   var sandbox = {}
   var resultKey = 'SAFE_EVAL_' + Math.floor(Math.random() * 1000000)
   sandbox[resultKey] = {}
-  code = resultKey + '=' + code
+  var clearContext = `
+    (function(){
+      Function = undefined;
+      const keys = Object.getOwnPropertyNames(this).concat(['constructor']);
+      keys.forEach((key) => {
+        const item = this[key];
+        if(!item || typeof item.constructor !== 'function') return;
+        this[key].constructor = undefined;
+      });
+    })();
+  `
+  code = clearContext + resultKey + '=' + code
   if (context) {
     Object.keys(context).forEach(function (key) {
       sandbox[key] = context[key]
