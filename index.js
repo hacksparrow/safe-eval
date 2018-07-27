e5999a3b297539df2521eb91acf89c8ce6f26114
Hage Yaapa <hage.yaapa@in.ibm.com>
2018-07-27 13:27:05 +0530

    lint

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ module.exports = function safeEval (code, context, opts) {
   var resultKey = 'SAFE_EVAL_' + Math.floor(Math.random() * 1000000)
   sandbox[resultKey] = {}
   var clearContext = `
-    (function(){
+    (function() {
       Function = undefined;
       const keys = Object.getOwnPropertyNames(this).concat(['constructor']);
       keys.forEach((key) => {
         const item = this[key];
-        if(!item || typeof item.constructor !== 'function') return;
+        if (!item || typeof item.constructor !== 'function') return;
         this[key].constructor = undefined;
       });
     })();
