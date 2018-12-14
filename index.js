23319e33f96b59ce547c847137ad751878dcbbd4
Hage Yaapa <captain@hacksparrow.com>
2018-12-14 19:05:28 +0530

    Revert "[CVE-2017-16088] Sandbox Breakout (Critical Security Fix) - context clear (#13)" (#14)
    This reverts commit 5e60f4a366b2fabc758801c1111d629076a5b8d3.

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 var vm = require('vm')
 
-function clearContext () {
-  // eslint-disable-next-line no-global-assign
-  Function = undefined
-  const keys = Object.getOwnPropertyNames(this).concat(['constructor'])
-  keys.forEach((key) => {
-    const item = this[key]
-    if (!item) return
-    if (typeof Object.getPrototypeOf(item).constructor === 'function') {
-      Object.getPrototypeOf(item).constructor = undefined
-    }
-    if (typeof item.constructor === 'function') {
-      this[key].constructor = undefined
-    }
-  })
-}
-
 module.exports = function safeEval (code, context, opts) {
   var sandbox = {}
   var resultKey = 'SAFE_EVAL_' + Math.floor(Math.random() * 1000000)
   sandbox[resultKey] = {}
-  var clearContextCall = `(${clearContext.toString()})();`
-  code = `${clearContextCall}${resultKey}=${code}`
+  var clearContext = `
+    (function() {
+      Function = undefined;
+      const keys = Object.getOwnPropertyNames(this).concat(['constructor']);
+      keys.forEach((key) => {
+        const item = this[key];
+        if (!item || typeof item.constructor !== 'function') return;
+        this[key].constructor = undefined;
+      });
+    })();
+  `
+  code = clearContext + resultKey + '=' + code
   if (context) {
     Object.keys(context).forEach(function (key) {
-      if (context[key] === Function) return
       sandbox[key] = context[key]
     })
   }
